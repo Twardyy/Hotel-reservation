e13b019a357696cceaea59773eebfbfaf336fad3
hubertgajdowski <240662@edu.p.lodz.pl>
2024-04-14 11:48:45 +0200

    Change RoomrRsponse to responeEntity

--- src/main/java/com/example/hotelreservation/room/RoomController.java
+++ src/main/java/com/example/hotelreservation/room/RoomController.java
@@ -3,8 +3,7 @@ package com.example.hotelreservation.room;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/room")
@@ -14,7 +13,7 @@ public class RoomController {
     public RoomController(final RoomService roomService) {this.roomService = roomService; }
 
     @PostMapping
-    ResponseEntity<?> addRoom(@RequestBody RoomRequest createRoomRequest) {
+    ResponseEntity<RoomResponse> addRoom(@RequestBody RoomRequest createRoomRequest) {
         if(isRoomAlreadyExists(createRoomRequest.roomNumber())){
             return ResponseEntity.status(CONFLICT).build();
         }
@@ -23,7 +22,9 @@ public class RoomController {
     }
 
     @GetMapping("/{id}")
-    RoomResponse fetchInformationAboutRoom(@PathVariable Long id) {return roomService.findRoomById(id); }
+    ResponseEntity<RoomResponse> fetchInformationAboutRoom(@PathVariable Long id) {
+        return ResponseEntity.status(OK).body(roomService.findRoomById(id));
+    }
     private boolean isRoomAlreadyExists(int roomNumber) {
         return roomService.findRoomByRoomNumber(roomNumber).isPresent();
     }
