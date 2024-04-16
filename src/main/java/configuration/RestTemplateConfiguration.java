fc1da57c1af8f432444059715ef98d309f041865
hubertgajdowski <240662@edu.p.lodz.pl>
2024-04-16 23:27:08 +0200

    Add swagger

--- src/main/java/configuration/RestTemplateConfiguration.java
+++ src/main/java/configuration/RestTemplateConfiguration.java
@@ -5,7 +5,6 @@ package configuration;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,4 +17,5 @@ public class RestTemplateConfiguration {
         restTemplate.setRequestFactory(requestFactory);
         return restTemplate;
     }
+
 }
